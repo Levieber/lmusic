ede26667b864dfcf6acc4782e8b17e7d38c978be
Levi Eber <gblevieber@gmail.com>
2023-09-25 16:37:39 -0300

    fix: fix prop server vs client warning

--- components/Playlists.tsx
+++ components/Playlists.tsx
@@ -5,8 +5,8 @@ export default function Playlists({ isOpen }: { isOpen: boolean }) {
 
   return (
     <aside
-      style={{ transform: isOpen ? "translateX(0%)" : "" }}
-      className="absolute right-0 w-96 h-[calc(100vh_-_7rem)] transition-transform border-r-4 border-black flex flex-col items-center translate-x-full bg-zinc-800 overflow-y-auto scrollbar-thin scrollbar-track-zinc-700 scrollbar-thumb-blue-400 scrollbar-track-rounded-full scrollbar-thumb-rounded-full"
+      style={{ transform: isOpen ? "translateX(0%)" : "translateX(100%)" }}
+      className="absolute right-0 w-96 h-[calc(100vh_-_7rem)] transition-transform border-r-4 border-black flex flex-col items-center bg-zinc-800 overflow-y-auto scrollbar-thin scrollbar-track-zinc-700 scrollbar-thumb-blue-400 scrollbar-track-rounded-full scrollbar-thumb-rounded-full"
     >
       <h2 className="p-10 text-3xl font-bold">Playlist:</h2>
       {musicPlaylist.length === 0 ? (
